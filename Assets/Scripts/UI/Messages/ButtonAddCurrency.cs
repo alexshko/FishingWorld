6052bcc61aa1a38b85496e95e41cfd06ca25dec2
alexshko <alexshko@gmail.com>
2021-03-04 21:01:15 +0200

    visual effects
    Visual effectts graph for winning currency (coins and diamonds).
    has a bug: raycast doesn't find the target commponent

--- Assets/Scripts/UI/Messages/ButtonAddCurrency.cs
+++ Assets/Scripts/UI/Messages/ButtonAddCurrency.cs
@@ -3,16 +3,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.VFX;
 
 namespace alexshko.fishingworld.UI.Messages
 {
     public enum Currency { Coins, Emeralds };
+
+
     public class ButtonAddCurrency : MonoBehaviour
     {
+        [Tooltip("the reference to the effect for when currency is added")]
+        public VisualEffect EffectRef;
+
+
         [SerializeField]
         private Currency currency;
         [SerializeField]
         private int amount;
+
+        private void Start()
+        {
+            if (Currency == Currency.Coins)
+            {
+                Vector3 worldResPos = FindPositionOfUIElement(transform.position);
+                Vector3 worldDesPos = FindPositionOfUIElement((UserStats.instance.CoinsRef.position));
+                EffectRef.SetVector3("EndPosition", worldDesPos);
+                EffectRef.transform.position = worldResPos;
+            }
+            else
+            {
+                Vector3 worldResPos = FindPositionOfUIElement((transform.position));
+                Vector3 worldDesPos = FindPositionOfUIElement((UserStats.instance.EmeraldsRef.position));
+                EffectRef.SetVector3("EndPosition", worldDesPos);
+                EffectRef.transform.position = worldResPos;
+            }
+        }
+
+        private Vector3 FindPositionOfUIElement(Vector3 pos)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(pos), out hit))
+            {
+                return hit.point;
+            }
+            return Vector3.zero;
+        }
 
         public Currency Currency { 
             get { return currency; } 
@@ -37,14 +72,20 @@ namespace alexshko.fishingworld.UI.Messages
             {
                 UserStats.instance.Emeralds += Amount;
             }
+            MakeAddCurrencyEffect();
+
 
             //hide the message:
-            transform.parent.gameObject.SetActive(false);
+            //transform.parent.gameObject.SetActive(false);
 
             //inform the GameManagement that the fishing cycle is finished:
-            GameManagement.Instance.FinishFishCaughtCycle();
+            //GameManagement.Instance.FinishFishCaughtCycle();
         }
 
-
+        private void MakeAddCurrencyEffect()
+        {
+            EffectRef.SendEvent("OnPlay");
+            EffectRef.Reinit();
+        }
     }
 }
