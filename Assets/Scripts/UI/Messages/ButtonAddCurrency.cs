3559bfb962b3d3c3d0f596e75a0930feddae143d
alexshko <alexshko@gmail.com>
2021-03-09 20:01:06 +0200

    .
    .

--- Assets/Scripts/UI/Messages/ButtonAddCurrency.cs
+++ Assets/Scripts/UI/Messages/ButtonAddCurrency.cs
@@ -21,15 +21,20 @@ namespace alexshko.fishingworld.UI.Messages
         [SerializeField]
         private int amount;
 
+        private Vector3 worldResPos;
+        public Transform originSphere;
+
         private void Start()
         {
             if (Currency == Currency.Coins)
             {
-                Vector3 worldResPos = transform.position;
+                worldResPos = transform.position;
                 Vector3 worldDesPos = (UserStats.instance.CoinsRef.parent.position);
                 worldDesPos.z -= 1;
+                worldResPos.z -= 1;
                 EffectRef.SetVector3("EndPosition", worldDesPos);
                 EffectRef.transform.position = worldResPos;
+                originSphere.transform.position = worldResPos;
             }
             else
             {
@@ -37,6 +42,15 @@ namespace alexshko.fishingworld.UI.Messages
                 Vector3 worldDesPos = FindPositionOfUIElement((UserStats.instance.EmeraldsRef.position));
                 EffectRef.SetVector3("EndPosition", worldDesPos);
                 EffectRef.transform.position = worldResPos;
+            }
+        }
+
+        private void OnDrawGizmos()
+        {
+            if (Currency == Currency.Coins)
+            {
+                Gizmos.color = Color.red;
+                Gizmos.DrawCube(worldResPos, new Vector3(10, 10, 10));
             }
         }
 
