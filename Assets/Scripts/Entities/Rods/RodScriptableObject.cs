93a2b4dbeaa977c2fdde4fb71ef3db2415db12d7
alexshko <alexshko@gmail.com>
2021-05-13 16:04:01 +0300

    scriptableRods

---
+++ Assets/Scripts/Entities/Rods/RodScriptableObject.cs
@@ -0,0 +1,20 @@
+﻿using alexshko.fishingworld.Enteties.Fishes;
+using System.Collections;
+using UnityEngine;
+
+namespace alexshko.fishingworld.Enteties.Rods
+{
+    [CreateAssetMenu(fileName = "Rod", menuName = "ScriptableObjects/RodScriptableObject", order = 2)]
+    public class RodScriptableObject : ScriptableObject
+    {
+
+        public int id;
+        public string Name;
+        public GameObject prefab;
+        public int CoinsPrice;
+        public int EmeraldsPrice;
+        public FishPopularity[] FishBonuses;
+        public float BoostCoins;
+        public float BoostEmeralds;
+    }
+}
