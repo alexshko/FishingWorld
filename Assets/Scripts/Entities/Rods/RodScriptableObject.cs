ac933d9757c373c827e8fd1f0362caf968aa8d77
alexshko <alexshko@gmail.com>
2021-05-13 19:56:35 +0300

    Update RodScriptableObject.cs

--- Assets/Scripts/Entities/Rods/RodScriptableObject.cs
+++ Assets/Scripts/Entities/Rods/RodScriptableObject.cs
@@ -7,14 +7,22 @@ namespace alexshko.fishingworld.Enteties.Rods
     [CreateAssetMenu(fileName = "Rod", menuName = "ScriptableObjects/RodScriptableObject", order = 2)]
     public class RodScriptableObject : ScriptableObject
     {
-
+        [Tooltip("the id of the Rod")]
         public int id;
+        [Tooltip("the name of the Rod")]
         public string Name;
+        [Tooltip("the prefab of the Rod defined by this Scriptable Object")]
         public GameObject prefab;
+        [Tooltip("the price of Rod in Coins")]
         public int CoinsPrice;
+        [Tooltip("the price of Rod in Emeralds")]
         public int EmeraldsPrice;
+        [Tooltip("the bonuses to catching Fish")]
         public FishPopularity[] FishBonuses;
+
+        [Tooltip("the boost to the coins revenue in every catch")]
         public float BoostCoins;
+        [Tooltip("the boost to the Emeralds revenue in every catch")]
         public float BoostEmeralds;
     }
 }
