6167477b5bbec02fcc48c58eaf1ef4b15a7bbe45
alexshko <alexshko@gmail.com>
2021-05-27 21:28:42 +0300

    .

---
+++ Assets/Scripts/UI/Messages/BuyItemMessageText.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuyItemMessageText : MonoBehaviour
+{
+    public Transform ItemNameRef;
+    public Transform ItemDescriptionRef;
+    public Transform ItemCoinsPriceRef;
+    public Transform ItemEmeraldsPriceRef;
+    public Image ItemImageRef;
+
+    private string itemName;
+    private string itemDescr;
+    private int coinsPrice;
+    private int emeraldsPrice;
+    private string imageLink;
+
+    public string ItemName
+    {
+        get => itemName;
+        set
+        {
+            itemName = value;
+            ItemNameRef.GetComponent<Text>().text = value;
+        }
+    }
+    public string ItemDescr
+    {
+        get => itemDescr;
+        set
+        {
+            itemDescr = value;
+            ItemDescriptionRef.GetComponent<Text>().text = value;
+        }
+    }
+}
